56c45f8c749fedc85b61f06c4472b0dbdbaf890e
VerdeDev <31958123+VerdiDev@users.noreply.github.com>
2021-08-16 15:32:44 +1000

    Added react-navigation with a template for use. Use yarn install to add missing dependencies.

--- PlantsVCars/App.js
+++ PlantsVCars/App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Use yarn install to add missing dependencies
 
-export default function App() {
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Text>This will be the home page :)</Text>
     </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  }
+})
+
+export default App;
