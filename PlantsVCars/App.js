6e0dd16f338e1e2d99c6cb1ff63fb5c4e4a23976
rwang031995 <75116221+rwang031995@users.noreply.github.com>
2021-08-19 15:35:35 +1000

    Changed import to screens from components.

--- PlantsVCars/App.js
+++ PlantsVCars/App.js
@@ -2,14 +2,58 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import PvCLeaderboard from "./Components/PvCLeaderboard";
-import PvCMyGardenNav from "./Components/PvCMyGarden";
+import { createStackNavigator } from '@react-navigation/stack';
+import PvCLeaderboard from './screens/Leaderboard';
+
 
 const Tab = createBottomTabNavigator();
 const seasons = ["Summer", "Autumn", "Winter", "Spring"];
+const Stack = createStackNavigator();
 
 var date = new Date();
 var season = seasons[Math.ceil((date.getMonth() + 1)/4)] // getMonth returns month from 0 - 11
+
+const MyCollection = () => {
+  return (
+    <View>
+      <Text> Collections page</Text>
+    </View>
+  )
+}
+
+const MyGardenNav = ({}) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name = "My Garden" component = {MyGarden} options = {{headerShown: false}}/>
+      <Stack.Screen name = "My Collection" component = {MyCollection}/>      
+    </Stack.Navigator>
+  )
+}
+const MyGarden = ({navigation}) => {
+  const [inventory, setInventory] = useState([]);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ alignItems: 'center', backgroundColor: "brown"}}>
+        <Button
+          title = "My Collection"
+          onPress = { () => navigation.navigate("My Collection")}
+        />
+      </View>
+      <View style={{ flex: 4, alignItems: 'center', justifyContent: 'center', backgroundColor: "lightgreen" }}>
+        <Text>
+          Current date: {date.toDateString() + "\n"}
+          Current season: {season + "\n"}
+        </Text>
+      </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "orange"}}>
+        <Text>
+          Inventory: {inventory.length == 0 ? "Empty" : inventory}
+        </Text>
+      </View>
+    </View>
+  )
+}
 
 const OurForest = () => {
   return (
@@ -31,9 +75,9 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="My Garden" component={PvCMyGardenNav} />
+        <Tab.Screen name="My Garden" component={MyGardenNav} />
         <Tab.Screen name="Our Forest" component={OurForest} />
-        <Tab.Screen name="Challenges" component={Challenges} />
+        <Tab.Screen name={"Challenges"} component={Challenges} />
         <Tab.Screen name="Leaderboard" component={PvCLeaderboard} />
       </Tab.Navigator>
     </NavigationContainer>
