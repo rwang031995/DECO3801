0a5650dc993e5e317b9886146a3cc206fc18fa85
VerdeDev <31958123+VerdiDev@users.noreply.github.com>
2021-08-19 12:06:44 +1000

    Fixed My Garden component

--- PlantsVCars/App.js
+++ PlantsVCars/App.js
@@ -13,48 +13,6 @@ const Stack = createStackNavigator();
 var date = new Date();
 var season = seasons[Math.ceil((date.getMonth() + 1)/4)] // getMonth returns month from 0 - 11
 
-const MyCollection = () => {
-  return (
-    <View>
-      <Text> Collections page</Text>
-    </View>
-  )
-}
-
-const MyGardenNav = ({}) => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name = "My Garden" component = {MyGarden} options = {{headerShown: false}}/>
-      <Stack.Screen name = "My Collection" component = {MyCollection}/>      
-    </Stack.Navigator>
-  )
-}
-const MyGarden = ({navigation}) => {
-  const [inventory, setInventory] = useState([]);
-
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={{ alignItems: 'center', backgroundColor: "brown"}}>
-        <Button
-          title = "My Collection"
-          onPress = { () => navigation.navigate("My Collection")}
-        />
-      </View>
-      <View style={{ flex: 4, alignItems: 'center', justifyContent: 'center', backgroundColor: "lightgreen" }}>
-        <Text>
-          Current date: {date.toDateString() + "\n"}
-          Current season: {season + "\n"}
-        </Text>
-      </View>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "orange"}}>
-        <Text>
-          Inventory: {inventory.length == 0 ? "Empty" : inventory}
-        </Text>
-      </View>
-    </View>
-  )
-}
-
 const OurForest = () => {
   return (
     <View style={styles.container}>
@@ -75,7 +33,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="My Garden" component={MyGardenNav} />
+        <Tab.Screen name="My Garden" component={PvCMyGardenNav} />
         <Tab.Screen name="Our Forest" component={OurForest} />
         <Tab.Screen name={"Challenges"} component={Challenges} />
         <Tab.Screen name="Leaderboard" component={PvCLeaderboard} />
