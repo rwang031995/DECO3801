07c5b8430f202e0c0237c920216698b7755862c9
MishKwok <michelle.kyh@hotmail.com>
2021-08-28 16:09:41 +1000

    added in the menu icons yay

--- PlantsVCars/App.js
+++ PlantsVCars/App.js
@@ -94,7 +94,29 @@ const OurForest = (images) => {
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route, }) => ({
+          tabBarIcon: ({image}) => {
+          if (route.name == 'My Garden') {
+            image = require('./images/icons/MyGardenIcon.png')
+          }
+          if (route.name == 'Our Forest') {
+            image = require('./images/icons/OurForestIcon.png')
+          }
+          if (route.name == 'Settings') {
+            image = require('./images/icons/SettingsIcon.png')
+          }          
+          if (route.name == 'Challenges') {
+            image = require('./images/icons/ChallengesIcon.png')
+          }
+          if (route.name == 'Leaderboard') {
+            image = require('./images/icons/LeaderboardIcon.png')
+          }
+          return (
+            <Image source={image} style={{width: 50, height:50}}/>
+          )}
+          })
+        }>
         <Tab.Screen name="My Garden" component={MyGardenNav} />
         <Tab.Screen name="Our Forest" component={OurForest} />
         <Tab.Screen name="Settings" component={SettingsNav} />        
