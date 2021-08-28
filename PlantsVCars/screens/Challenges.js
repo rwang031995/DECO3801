b4d5f939cbd1f4e4f6a66d6423edd61dd72a56a9
rwang031995 <75116221+rwang031995@users.noreply.github.com>
2021-08-28 19:17:03 +1000

    level now stores data globally. Added comments for sectioning each element.

--- PlantsVCars/screens/Challenges.js
+++ PlantsVCars/screens/Challenges.js
@@ -1,14 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import moment from 'moment';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ChallengeOptions = ["Challenge 1", "Challenge 2", "Challenge 3", "Challenge 4", "Challenge 5"]
 
 const ChallengesScreen = () => {
+    /**
+     * Variables for global storage using Async.
+     */
     const [level, setLevel] = useState(1);
     const [challenges, isComplete] = useState(false);
     const selectedChallenges = [];
     const [currentWeek, changeWeek] = useState(moment().clone().startOf('isoWeek').add(1, 'days'));
+
+    //--------------------------------------------------------------------------------
+
+    /**
+     * save globally stored level. 
+     */
+    const saveLevel = async(x) => {
+        try {
+            await AsyncStorage.setItem("MyLevel", JSON.stringify(x));
+            console.log(x);
+        } catch (err) {
+            console.log("save level error");
+        }
+    }
+
+    /**
+     * load globally stored level. 
+     */
+    const loadLevel = async() => {
+        try {
+            ourLevel = await AsyncStorage.getItem("MyLevel");
+            if (ourLevel != null) {
+                setLevel(JSON.parse(ourLevel));
+            }
+        } catch (err) {
+            console.log("load level error");
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+
+    /**
+     * save globally stored week. 
+     */
+     const saveWeek = async() => {
+        try {
+            await AsyncStorage.setItem("currentWeek", JSON.stringify(x));
+            console.log(x);
+        } catch (err) {
+                console.log("save level error");
+        }
+    }
+    
+    /**
+     * load globally stored week. 
+     */
+    const loadWeek = async() => {
+        try {
+            ourLevel = await AsyncStorage.getItem("MyLevel");
+            if (ourLevel != null) {
+                setLevel(JSON.parse(ourLevel));
+            }
+        } catch (err) {
+            console.log("load level error");
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+
+    /**
+     * Code for generating, storying and loading challenges.
+     */
 
     //Generates a random number that is used to random select challenges from a pool.
     const generateNumber = () => {
@@ -18,51 +84,52 @@ const ChallengesScreen = () => {
 
     const levelTo = (x) => {
         setLevel(x);
+        saveLevel(x);
     }
 
     const nextWeek = () => {
         return moment().isSameOrAfter(currentWeek.add(7, 'days'));
     }
 
+    //--------------------------------------------------------------------------------
+
+    /**
+     * Load all the globally stored data upon opening page.
+     */
+    useEffect(() => {
+        loadLevel();
+    }, []);
+
+    //--------------------------------------------------------------------------------
+
+    /**
+     * View screen
+     */
+
     if (level == 1) {
-        selectedChallenges.push(ChallengeOptions[generateNumber()]);
-        console.log(selectedChallenges[0]);
-        console.log(nextWeek());
         return (
             <View style={styles.container}>
                 <Text> Level {level}</Text>
-                <Text> Challenges</Text>
-                <Text> Challenge 1 = {selectedChallenges[0]}</Text>
                 <Button title="Level test" onPress = {() => {levelTo(2)}}/>
-                <Button title="Random generator" onPress = {() => {levelTo(1)}}/>
                 <Text> Bonus Challenges </Text>
             </View>
         )
     } else if (level == 2) {
-        selectedChallenges.push(ChallengeOptions[generateNumber()]);
-        selectedChallenges.push(ChallengeOptions[generateNumber()]);
-        console.log(selectedChallenges);
         return (
             <View style={styles.container}>
                 <Text> Level {level}</Text>
-                <Text> Challenges </Text>
-                <Text> Challenge 1 = {selectedChallenges[0]}</Text>
-                <Text> Challenge 2 = {selectedChallenges[1]}</Text>
                 <Button title="Level test" onPress = {() => {levelTo(1)}}/>
-                <Button title="Random generator" onPress = {() => {levelTo(1)}}/>
                 <Text> Bonus Challenges </Text>
             </View>
         )
-    } else if (level == 3) {
-        return (
-            <View style={styles.container}>
-                <Text> Level {level}</Text>
-                <Text> Challenges </Text>
-                <Text> Bonus Challenges </Text>
-            </View>
-        )
-    } 
+    }
 }
+
+    //--------------------------------------------------------------------------------
+
+    /**
+     * Styling code
+     */
 
 const styles = StyleSheet.create({
     container: {
