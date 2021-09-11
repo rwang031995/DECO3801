0f65d4ca2a8367ed950be139c8894ec5e03ace81
MishKwok <michelle.kyh@hotmail.com>
2021-09-11 23:54:04 +1000

    partially implemented pinch and zoom

--- PlantsVCars/screens/garden/MyGarden.js
+++ PlantsVCars/screens/garden/MyGarden.js
@@ -1,7 +1,10 @@
-import {Text, View, Button, Image, StyleSheet} from "react-native";
+import {Text, View, Button, Image, StyleSheet, Dimensions} from "react-native";
 import React, {useState} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
+
+//RETRIEVED FROM https://morioh.com/p/e42eec224939
+import ImageZoom from 'react-native-image-pan-zoom'
 
 import Inventory from "./Inventory";
 import {img} from "../../images/manifest"
@@ -10,6 +13,9 @@ const seasons = ["Summer", "Autumn", "Winter", "Spring"];
 
 var date = new Date();
 var season = seasons[Math.ceil((date.getMonth() + 1)/4)]; // getMonth returns month from 0 - 11
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const Stack = createStackNavigator();
 
@@ -36,16 +42,20 @@ const styles = StyleSheet.create({
       height: "33%",
     },
     plantTile: {
-        height: "25%",
-        width: "25%",
+        height: "20%",
+        width: "20%",
         margin: "4%"
+    },
+    overallBG: {
+        height: windowHeight,
+        width: windowWidth
     }
   })
 
 const MyGarden = ({navigation}) => {
   const [inventory, setInventory] = useState([]);
   
-  var seasonBG = (
+  /*var seasonBG = (
     <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
         {img({name: season+"-top", style: styles.bgTile})}
         {img({name: season+"-duck", style: styles.bgTile})}
@@ -62,18 +72,21 @@ const MyGarden = ({navigation}) => {
         {img({name: season, style: styles.bgTile})}
         {img({name: season, style: styles.bgTile})}
     </View> 
-  );
+  );*/
   
   var plantsInGround = (
     // TODO: make this dynamic somehow
     <View style={{flex: 0, flexDirection: 'row', flexWrap: 'wrap', height: "100%", 
-        justifyContent: "space-around", position: 'absolute', top: '33%'}}>
-        {img({name: "DandelionFlower", style: styles.plantTile})}
-        {img({name: "RoseFlower", style: styles.plantTile})}
-        {img({name: "OrchidFlower", style: styles.plantTile})}
-        {img({name: "RoseFlower", style: styles.plantTile})}
-        {img({name: "OrchidFlower", style: styles.plantTile})}
-        {img({name: "TulipFlower", style: styles.plantTile})}
+        justifyContent: "space-around", position: 'absolute', top: '25%'}}>
+        {img({name: "Barren", style: styles.plantTile})}
+        {img({name: "Barren", style: styles.plantTile})}
+        {img({name: "Barren", style: styles.plantTile})}
+        {img({name: "Barren", style: styles.plantTile})}
+        {img({name: "Barren", style: styles.plantTile})}
+        {img({name: "Barren", style: styles.plantTile})}
+        {img({name: "Barren", style: styles.plantTile})}
+        {img({name: "Barren", style: styles.plantTile})}
+        {img({name: "Barren", style: styles.plantTile})}
     </View>
   );
 
@@ -85,15 +98,26 @@ const MyGarden = ({navigation}) => {
           onPress = { () => navigation.navigate("My Collection")}
         />
       </View>
-      <View style={{ flex:4, backgroundColor: "lightgreen", display:'flex'}}>
-        {seasonBG}
+      <View style={{ flex:4, backgroundColor: "lightgreen"}}>
+        <ImageZoom cropWidth="100%"
+                   cropHeight="100%"
+                   imageWidth={"100%"}
+                   imageHeight={"100%"}>
+
+                    {img({name: season+"-bg", style: styles.overallBG})}
+
+                    <View style={{position:"absolute", height:"100%", width:"100%"}}>
+                      {plantsInGround}
+                    </View>
+                  
+        </ImageZoom>
         <View style={{position: 'absolute', justifyContent:'center'}}>       
           <Text>
             Current date: {date.toDateString() + "\n"}
             Current season: {season + "\n"}
           </Text>
         </View>
-        {plantsInGround}
+        
       </View> 
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "orange"}}>
         <Text>
