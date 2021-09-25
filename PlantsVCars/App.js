681ea75511daa60a0559fc744d8a4ee7e56215d6
rwang031995 <75116221+rwang031995@users.noreply.github.com>
2021-09-25 16:35:06 +1000

    Functional loginpage and signin/out option

--- PlantsVCars/App.js
+++ PlantsVCars/App.js
@@ -8,7 +8,8 @@ import PvCLeaderboard from './screens/Leaderboard';
 import MyGardenNav from './screens/garden/MyGarden'
 import ChallengesScreen from './screens/Challenges';
 import SettingsNav from "./screens/settings/Settings";
-import LoginScreen from './screens/settings/Login';
+import { TextInput } from 'react-native-gesture-handler';
+import { auth } from './screens/settings/Firebase';
 
 
 // Adding to real time database code.1
@@ -62,7 +63,19 @@ const styles = StyleSheet.create({
   bg : {
     width: '100%',
     height: '100%',
-  }
+  },
+  input: {
+    fontSize: 20,
+    width: '60%',
+    borderRadius: 5,
+    backgroundColor: 'lightgrey',
+    paddingVertical: 10,     
+    paddingHorizontal: 15, 
+  },
+  category: {
+      fontSize: 30,
+      paddingVertical: 15,
+  },
 })
 
 var images = [
@@ -110,13 +123,108 @@ const OurForest = (images) => {
     );
 }
 
-console.log(LoginScreen.getUser());
-
 const App = () => {
 
-  if (1) {
+  /**
+   * Login with email & password.
+   */
+  const [initializing, setInitializing] = useState(true);
+  const [userEmail, setUserEmail] = useState(null);
+  const [user, setUser] = useState(null);
+  const [password, setPassword] = useState(null);
+   
+  const onAuthStateChanged = (user) => {
+    setUser(user);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+ 
+  useEffect(() => {
+    const subscriber = auth.onAuthStateChanged(onAuthStateChanged);
+      return subscriber
+    }, [])
+ 
+  if (initializing) {
+    return null;
+  }
+ 
+  const signIn = (email, password) => {
+    auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log('logged in');
+    })
+    .catch(error => {
+      if (error.code === 'auth/user-not-found') {
+        alert("User not found");
+      }
+      if (error.code === 'auth/invalid-email') {
+        alert("Email is incorrect");
+      }
+      if (error.code === 'auth/wrong-password') {
+        alert("Incorrect password");
+      }
+      if (error.code === 'auth/user-disabled') {
+        alert("User has been disabled");
+      }
+      console.log(error);
+    })
+  }
+     
+  const createUser = (email, password) => {
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log('User account has been created & signed in!')
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log("email already in use");
+        alert("This email already has an account");
+      } 
+      if (error.code === 'auth/weak-password') {
+        alert("password is too weak");
+      }
+      if (error.code === 'auth/invalid-email') {
+        console.log("email invalid");
+        alert("Invalid email");
+      }
+      console.log(error);
+    })
+  }
+     
+  // auth.signOut()
+  // .then(() => console.log('User signed out!'));
+
+
+  if (!user) {
     return (
-      <LoginScreen></LoginScreen>
+      <View style = {styles.container}>
+        <Text style = {styles.category}>Username</Text>
+        <TextInput style = {styles.input}
+            placeholder='example@gmail.com'
+            onChangeText = {text => setUserEmail(text)}
+            autoCapitalize = "none"
+            autoCompleteType = "off"
+            autoCorrect = {false}
+        />
+        <Text style = {styles.category}>Password</Text>
+        <TextInput style = {styles.input}
+            placeholder='password'
+            secureTextEntry = {true}
+            onChangeText = {text => setPassword(text)}
+            autoCapitalize = {false}
+            autoCompleteType = {false}
+            autoCorrect = {false}
+        />
+        <Button
+            title = "Login"
+            onPress = {() => signIn(userEmail, password)}
+        />
+        <Button
+            title = "Create Account"
+            onPress = {() => createUser(userEmail, password)}
+        />
+      </View>
     )
   }
 
