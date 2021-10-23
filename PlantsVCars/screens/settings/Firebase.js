f36f9b60a0eac06f249f4aea5db228a87145597d
jayportwood <jaydenportwood@gmail.com>
2021-10-24 02:10:10 +1000

    Revert back to roger's db

--- PlantsVCars/screens/settings/Firebase.js
+++ PlantsVCars/screens/settings/Firebase.js
@@ -6,26 +6,27 @@ import "@firebase/firestore"
  */
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCR5WDI8njkxgAJ67fZbhLx07gO6dy0eK0",
-//     authDomain: "plantsva-429e2.firebaseapp.com",
-//     projectId: "plantsva-429e2",
-//     storageBucket: "plantsva-429e2.appspot.com",
-//     messagingSenderId: "1001065166477",
-//     appId: "1:1001065166477:web:9968b8cf3e3ed9668114c4",
-//     measurementId: "G-5EZJFLMLTE"
-// };
-   
+
 const firebaseConfig = {
- apiKey: "AIzaSyDpXBMJkITPHT0zqfe_pOA8XgXP7ba7_UE",
- authDomain: "plantsvscars-26ad0.firebaseapp.com",
- databaseURL: "https://plantsvscars-26ad0-default-rtdb.firebaseio.com",
- projectId: "plantsvscars-26ad0",
- storageBucket: "plantsvscars-26ad0.appspot.com",
- messagingSenderId: "857742504179",
- appId: "1:857742504179:web:eae1227c852f29a3f44786",
- measurementId: "G-WRPQE0R2B5"
+    apiKey: "AIzaSyCR5WDI8njkxgAJ67fZbhLx07gO6dy0eK0",
+    authDomain: "plantsva-429e2.firebaseapp.com",
+    projectId: "plantsva-429e2",
+    storageBucket: "plantsva-429e2.appspot.com",
+    messagingSenderId: "1001065166477",
+    appId: "1:1001065166477:web:9968b8cf3e3ed9668114c4",
+    measurementId: "G-5EZJFLMLTE"
 };
+   
+// const firebaseConfig = {
+//  apiKey: "AIzaSyDpXBMJkITPHT0zqfe_pOA8XgXP7ba7_UE",
+//  authDomain: "plantsvscars-26ad0.firebaseapp.com",
+//  databaseURL: "https://plantsvscars-26ad0-default-rtdb.firebaseio.com",
+//  projectId: "plantsvscars-26ad0",
+//  storageBucket: "plantsvscars-26ad0.appspot.com",
+//  messagingSenderId: "857742504179",
+//  appId: "1:857742504179:web:eae1227c852f29a3f44786",
+//  measurementId: "G-WRPQE0R2B5"
+// };
 
 if (!firebase.apps.length) {
  firebase.initializeApp(firebaseConfig)
